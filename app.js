9a423fa2c5ca5e7582e353204a9e869b6e0e6d82
jatins13 <js23@iitbbs.ac.in>
2020-02-23 15:25:06 +0530

    Without DB

---
+++ app.js
@@ -0,0 +1,13 @@
+var express= require('express');
+var app = express();
+var todoController= require('./controllers/todoController.js');
+
+//setting up template engine
+app.set('view engine', 'ejs');
+
+//route for static files
+app.use(express.static('./public'));
+
+todoController(app);
+
+app.listen(3000);
---
+++ controllers/todoController.js
@@ -0,0 +1,26 @@
+var bodyParser=require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false}); 
+
+var data = [{task:'get'},{task:'cook food'}, {task:'code till death'}];
+
+module.exports = function(app) {
+
+    app.get(('/todo'), function(req, res){
+        res.render('todo', {todos: data});
+
+    });
+    app.post(('/todo'),urlencodedParser, function(req, res){
+        data.push(req.body);
+        console.log(req.body);
+        res.json(data);
+
+    });
+
+    app.delete(('/todo/:item'), function(req, res){
+        data= data.filter(function(todo){
+            return todo.task.replace(/ /g, '-') !== req.params.item
+        });
+        res.json({todos:data});    
+
+    });
+}
